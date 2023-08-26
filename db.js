7e5f57da4a35f8e78a5cbebf3e8da3c2b68a3094
yashdarji30 <123467783+yashdarji30@users.noreply.github.com>
2023-08-26 16:59:44 +0530

    Add files via upload

---
+++ db.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/medicine_stock', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', () => {
+  console.log('Connected to the database');
+});
+
+module.exports = db;
